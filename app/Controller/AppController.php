901fce5400f1f15591ac66b7960e48e564df7aed
Christopher Castro <y2k2000@gmail.com>
2011-08-12 12:09:54 +0200

    updating @package annotation

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5
  *
- * @package  QuickApps
+ * @package  QuickApps.Controller
  * @version  1.0
  * @author   Christopher Castro <chris@quickapps.es>
  * @link     http://cms.quickapps.es
